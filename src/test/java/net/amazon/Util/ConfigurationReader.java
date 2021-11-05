927f1019635d0deee00d00a987caed408261bb15
Olena_L <lytvynolena@gmail.com>
2021-11-05 10:18:50 -0500

    Initial commit

---
+++ src/test/java/net/amazon/Util/ConfigurationReader.java
@@ -0,0 +1,31 @@
+package net.amazon.Util;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigurationReader {
+
+    private static Properties properties = new Properties();
+
+    static {
+
+        try {
+
+            FileInputStream file = new FileInputStream("configuration.properties");
+
+            properties.load(file);
+
+            file.close();
+        } catch (IOException e) {
+
+            System.out.println("File is not found in Configuration properties. ");
+        }
+    }
+
+    public static String getProperty(String keyWord){
+
+        return properties.getProperty(keyWord);
+    }
+
+}
