08680f1f328ddee60cc08411f55984a436eb5735
Khrystyna B22 <khprokhorenko@gmail.com>
2021-11-05 11:57:51 -0500

    added CukesRunner and implement these steps using the snippet(s)

---
+++ src/test/java/net/amazon/step_definitionsK/Login_AmazonK.java
@@ -0,0 +1,25 @@
+package net.amazon.step_definitionsK;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Login_AmazonK {
+
+    @When("user enters username")
+    public void user_enters_username() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("user enters password")
+    public void user_enters_password() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("user should see dashboard")
+    public void user_should_see_dashboard() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+}
--- src/test/resources/features/login.feature
+++ src/test/resources/features/login.feature
@@ -1,7 +1,7 @@
 Feature: amazon app login
   User Story:
   As a user, I should be able to login with correct credentials
-
+@wip
   Scenario: login as a user
     When user enters username
     And user enters password
